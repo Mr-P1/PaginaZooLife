d9fc391c18f4b02368cd16c55421c8d276272b97
Mr-P1 <jo.salvo@duocuc.cl>
2024-09-24 22:05:46 -0300

    Alerta Cerrar sesión
    Se modifico el cerrar sesión para que salte una alerta por si esta seguro de querer cerrar la sesión

--- src/app/Paginas/base/base.component.ts
+++ src/app/Paginas/base/base.component.ts
@@ -2,6 +2,10 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink ,Router, RouterModule} from '@angular/router';
 import {AuthStateService} from '../../data-acces/auth-state.service'
 
+// Sweetalert2
+import Swal from 'sweetalert2';
+
+
 @Component({
   selector: 'app-base',
   standalone: true,
@@ -15,9 +19,32 @@ export class BaseComponent {
   private _authState = inject(AuthStateService);
   private _router = inject(Router);
 
-  async logOut(){
+  async logOut2(){
     await this._authState.logOut();
     this._router.navigate(['']);
   }
 
+  async logOut() {
+    const result = await Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'Se cerrara la sesión actual.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, Cerrar sesión',
+      cancelButtonText: 'Volver',
+      backdrop: 'rgba(0, 0, 0, 0.8)',
+    });
+
+    if (result.isConfirmed) {
+      try {
+
+        await this._authState.logOut();
+        this._router.navigate(['']);
+
+
+      } catch {
+
+      }
+    }
+  }
 }
